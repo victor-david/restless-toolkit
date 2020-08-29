544ee78bee53e93214e1c9c9045533b835748b9a
Victor D. Sandiego <codepuppy@balticsoap.com>
2020-08-28 22:09:50 -0500

    Add custom list box

---
+++ src/Restless.Toolkit/Controls/ListBoxItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace Restless.Toolkit.Controls
+{
+    /// <summary>
+    /// Represents a list box item that ignores the right mouse button.
+    /// </summary>
+    public class ListBoxItem : System.Windows.Controls.ListBoxItem
+    {
+        /// <summary>
+        /// Occurs when the right mouse button is pressed. To surpress right click
+        /// selection of the item, this method does nothing.
+        /// </summary>
+        /// <param name="e">The event args</param>
+        protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+        }
+    }
+}
