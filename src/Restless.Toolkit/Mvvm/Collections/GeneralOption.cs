ab96ea53cbc5e27cab0356f1c0c16425855eaa39
Victor D. Sandiego <codepuppy@balticsoap.com>
2021-03-12 20:20:33 -0600

    Add Mvvm helper classes

---
+++ src/Restless.Toolkit/Mvvm/Collections/GeneralOption.cs
@@ -0,0 +1,48 @@
+﻿namespace Restless.Toolkit.Mvvm.Collections
+{
+    /// <summary>
+    /// Represents a general purpose helper class that can be used when binding.
+    /// </summary>
+    public class GeneralOption : SelectableItem
+    {
+        #region Public properties
+        /// <summary>
+        /// Gets or sets a string value associated with this option.
+        /// </summary>
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets an integer value associated with this option.
+        /// </summary>
+        public long IntValue
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets an integer value associated with this option.
+        /// </summary>
+        public long IntValue2
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        /************************************************************************/
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeneralOption"/> class.
+        /// </summary>
+        public GeneralOption()
+        {
+        }
+        #endregion
+    }
+}
