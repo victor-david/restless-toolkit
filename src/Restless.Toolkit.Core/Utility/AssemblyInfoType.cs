4c8e951c67b7ea0a6b4f231197c0d83577f2f511
Victor D. Sandiego <codepuppy@balticsoap.com>
2021-03-13 18:50:55 -0600

    Add AssemblyInfo class (Restless.Toolkit.Core)

---
+++ src/Restless.Toolkit.Core/Utility/AssemblyInfoType.cs
@@ -0,0 +1,21 @@
+﻿namespace Restless.Toolkit.Core.Utility
+{
+    /// <summary>
+    /// Provides an enumeration for instaniating an <see cref="AssemblyInfo"/>object.
+    /// </summary>
+    public enum AssemblyInfoType
+    {
+        /// <summary>
+        /// Information is retrieved from the entry assembly.
+        /// </summary>
+        Entry,
+        /// <summary>
+        /// Information is retrieved from the calling assembly.
+        /// </summary>
+        Calling,
+        /// <summary>
+        /// Information is retrieved from the executing assembly.
+        /// </summary>
+        Executing
+    }
+}
