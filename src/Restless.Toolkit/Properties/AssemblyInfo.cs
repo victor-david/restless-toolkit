745858843d2edc7f3badca2dcf7df45a3369beca
Victor D. Sandiego <codepuppy@balticsoap.com>
2021-03-17 09:17:37 -0600

    Update assembly info
    Update assembly info to provide Xmlns definitions

---
+++ src/Restless.Toolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+using System.Windows.Markup;
+/* ThemeInfo(1,2)
+* 1. where theme specific resource dictionaries are located (used if a resource is not found in the page, or application resource dictionaries)
+* 2. where the generic resource dictionary is located (used if a resource is not found in the page, app, or any theme specific resource dictionaries)
+*/
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: XmlnsPrefix("http://schemas.restless.toolkit.com/2021", "toolkit")]
+[assembly: XmlnsDefinition("http://schemas.restless.toolkit.com/2021", "Restless.Toolkit.Controls")]
+[assembly: XmlnsDefinition("http://schemas.restless.toolkit.com/2021", "Restless.Toolkit.Converters")]
+[assembly: XmlnsDefinition("http://schemas.restless.toolkit.com/2021", "Restless.Toolkit.Core")]
+[assembly: XmlnsDefinition("http://schemas.restless.toolkit.com/2021", "Restless.Toolkit.Mvvm")]
+[assembly: XmlnsDefinition("http://schemas.restless.toolkit.com/2021", "Restless.Toolkit.Utility")]
