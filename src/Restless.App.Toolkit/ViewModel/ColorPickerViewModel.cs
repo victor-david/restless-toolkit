e295b09872296f17dfe28803eff068efe5c623f1
Victor D. Sandiego <codepuppy@balticsoap.com>
2021-04-14 20:22:20 -0500

    Update demo to display/hide sliders
    Update the color picker demo to show how the sliders may be configured to display or hide.

--- src/Restless.App.Toolkit/ViewModel/ColorPickerViewModel.cs
+++ src/Restless.App.Toolkit/ViewModel/ColorPickerViewModel.cs
@@ -8,6 +8,9 @@ namespace Restless.App.Toolkit
     {
         private double canvasWidth;
         private double sliderSize;
+        private ColorSliderConfig sliderConfig;
+        private bool displayRgbSliders;
+        private bool displayAlphaSlider;
         private Color selectedColor;
 
         public ColorPickerViewModel()
@@ -17,6 +20,8 @@ namespace Restless.App.Toolkit
             CanvasWidth = ColorPicker.DefaultCanvasWidth;
             SliderSize = ColorSlider.DefaultSliderSize;
             SelectedColor = ColorValues.DefaultColor;
+            DisplayRgbSliders = true;
+            DisplayAlphaSlider = true;
         }
 
         /// <summary>
@@ -38,12 +43,55 @@ namespace Restless.App.Toolkit
         }
 
         /// <summary>
+        /// Gets the value that configures the sliders for the color picker control.
+        /// </summary>
+        public ColorSliderConfig SliderConfig
+        {
+            get => sliderConfig;
+            private set => SetProperty(ref sliderConfig, value);
+        }
+
+        /// <summary>
+        /// Gets or sets a boolean value that determines if the RGB sliders are displayed.
+        /// </summary>
+        public bool DisplayRgbSliders
+        {
+            get => displayRgbSliders;
+            set
+            {
+                SetProperty(ref displayRgbSliders, value);
+                UpdateSliderConfig();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a boolean value that determines if the alpha slider is displayed.
+        /// </summary>
+        public bool DisplayAlphaSlider
+        {
+            get => displayAlphaSlider;
+            set
+            {
+                SetProperty(ref displayAlphaSlider, value);
+                UpdateSliderConfig();
+            }
+        }
+
+        /// <summary>
         /// Gets or sets the color for the view.
         /// </summary>
         public Color SelectedColor
         {
             get => selectedColor;
             set => SetProperty(ref selectedColor, value);
+        }
+
+        private void UpdateSliderConfig()
+        {
+            ColorSliderConfig value = ColorSliderConfig.None;
+            if (DisplayRgbSliders) value |= ColorSliderConfig.Rgb;
+            if (DisplayAlphaSlider) value |= ColorSliderConfig.Alpha;
+            SliderConfig = value;
         }
 
         private void RunSetColorCommand(object parm)
