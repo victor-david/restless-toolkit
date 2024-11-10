0e4e1428928a87d05280d6a3a3509ef27ad8b38a
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-11-10 10:30:26 -0600

    Make Messages obsolete

--- src/Restless.Toolkit/Utility/Messages.cs
+++ src/Restless.Toolkit/Utility/Messages.cs
@@ -1,4 +1,4 @@
-﻿using Restless.Toolkit.InternalResources;
+﻿using System;
 using System.Windows;
 
 namespace Restless.Toolkit.Utility
@@ -6,16 +6,19 @@ namespace Restless.Toolkit.Utility
     /// <summary>
     /// Provides static utility methods to display messages.
     /// </summary>
+    [Obsolete("Use MessageWindow instead")]
     public static class Messages
     {
         /// <summary>
         /// Displays a dialog box with Yes / No buttons.
         /// </summary>
         /// <param name="message">The message to display in the dialog box.</param>
+        /// <param name="caption">The caption, or null to use the default</param>
         /// <returns>true if Yes if selected; otherwise, false.</returns>
-        public static bool ShowYesNo(string message)
+        public static bool ShowYesNo(string message, string caption = null)
         {
-            MessageBoxResult result = MessageBox.Show(message, Strings.CaptionConfirm, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            caption = string.IsNullOrWhiteSpace(caption) ? "Confirm" : caption;
+            MessageBoxResult result = MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
             return (result == MessageBoxResult.Yes);
         }
 
@@ -23,18 +26,22 @@ namespace Restless.Toolkit.Utility
         /// Display a dialog box with an error icon.
         /// </summary>
         /// <param name="message">The message to display in the dialog box.</param>
-        public static void ShowError(string message)
+        /// <param name="caption">The caption, or null to use the default</param>
+        public static void ShowError(string message, string caption = null)
         {
-            MessageBox.Show(message, Strings.CaptionOperationNotAvailable, MessageBoxButton.OK, MessageBoxImage.Error);
+            caption = string.IsNullOrWhiteSpace(caption) ? "Operation Not Available" : caption;
+            MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
         /// <summary>
         /// Display a dialog box with an information icon.
         /// </summary>
         /// <param name="message">The message to display in the dialog box.</param>
-        public static void Show(string message)
+        /// <param name="caption">The caption, or null to use the default</param>
+        public static void Show(string message, string caption = null)
         {
-            MessageBox.Show(message, Strings.CaptionInformation, MessageBoxButton.OK, MessageBoxImage.Information);
+            caption = string.IsNullOrWhiteSpace(caption) ? "Information" : caption;
+            MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
