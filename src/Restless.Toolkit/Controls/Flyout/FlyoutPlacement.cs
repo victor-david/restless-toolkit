5287ebeb09f7fff1296d63801bbf9895c9cb643a
Victor D. Sandiego <codepuppy@balticsoap.com>
2024-11-16 15:38:01 -0600

    Add flyout

---
+++ src/Restless.Toolkit/Controls/Flyout/FlyoutPlacement.cs
@@ -0,0 +1,42 @@
+﻿namespace Restless.Toolkit.Controls
+{
+    /// <summary>
+    /// Describes the position for a <see cref="Flyout"/>
+    /// </summary>
+    public enum FlyoutPlacement
+    {
+        /// <summary>
+        /// Left (full)
+        /// </summary>
+        Left,
+        /// <summary>
+        /// Top left
+        /// </summary>
+        TopLeft,
+        /// <summary>
+        /// Center left
+        /// </summary>
+        CenterLeft,
+        /// <summary>
+        /// Bottom left
+        /// </summary>
+        BottomLeft,
+
+        /// <summary>
+        /// Right (full)
+        /// </summary>
+        Right,
+        /// <summary>
+        /// Top right
+        /// </summary>
+        TopRight,
+        /// <summary>
+        /// Center right
+        /// </summary>
+        CenterRight,
+        /// <summary>
+        /// Bottom right
+        /// </summary>
+        BottomRight
+    }
+}
